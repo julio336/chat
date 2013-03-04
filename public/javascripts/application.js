cef792e9c89cd2f69ce819f6fe5a9261f1b39a32
julio <julio336@hotmail.com>
2013-03-03 22:02:23 -0600

    faye

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://localhost:9292/faye');
+  var faye = new Faye.Client('http://chat99.herokuapp.com/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
